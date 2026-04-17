f3c8e096d7acb6cc645f7853d02da93acae8ac2d
Vivek Verma <vivekvermaiit@gmail.com>
2026-04-17 12:45:45 -0400

    notarization changes

---
+++ electron/scripts/notarize.js
@@ -0,0 +1,16 @@
+const { notarize } = require('@electron/notarize');
+
+exports.default = async function notarizing(context) {
+  const { electronPlatformName, appOutDir } = context;
+  if (electronPlatformName !== 'darwin') return;
+
+  const appName = context.packager.appInfo.productFilename;
+
+  await notarize({
+    tool: 'notarytool',
+    appPath: `${appOutDir}/${appName}.dmg`,
+    appleId: process.env.APPLE_ID,
+    appleIdPassword: process.env.APPLE_ID_PASSWORD,
+    teamId: process.env.APPLE_TEAM_ID,
+  });
+};
